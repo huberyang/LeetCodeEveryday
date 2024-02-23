6c227e19223978c1a2b5077bd9ebfe1f8a7ff6a9
YangSen <yang.sen@ufinity.com>
2024-02-23 11:52:51 +0800

    [day2][addTwoNumbers] completed the implement

--- day2-addTwoNumbers/src/main/java/MySolution.java
+++ day2-addTwoNumbers/src/main/java/MySolution.java
@@ -1,8 +1,24 @@
+import java.util.Objects;
+
 public class MySolution {
-  public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 
 
-    return null;
+  public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    ListNode result = new ListNode();
+    ListNode pointer = result;
+    int curry = 0;
+    while (Objects.nonNull(l1) || Objects.nonNull(l2) || curry == 1) {
+      int sum = (l1 != null ? l1.val : 0) + (l2 != null ? l2.val : 0) + curry;
+
+      curry = sum / 10;
+
+      l1 = l1 != null ? l1.next : null;
+      l2 = l2 != null ? l2.next : null;
+
+      ListNode next = new ListNode(sum % 10);
+      pointer.next = next;
+      pointer = pointer.next;
+    }
+    return result.next;
   }
-
 }
--- day2-addTwoNumbers/src/test/java/TestCase.java
+++ day2-addTwoNumbers/src/test/java/TestCase.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class TestCase {
+import java.util.Arrays;
+import org.junit.Test;
+
+public class TestCase {
+
+  @Test
+  public void testCaseOne(){
+
+  }
+
 }
