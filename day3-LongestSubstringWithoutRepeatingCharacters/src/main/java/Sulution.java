87be0920fc0b4617b39a7589930400a8080f8dc8
YangSen <yang.sen@ufinity.com>
2024-02-23 16:27:41 +0800

    [day3][longestSubstringWithoutRepeatingCharacters] complede the solution and update for the README.md

--- day3-LongestSubstringWithoutRepeatingCharacters/src/main/java/Sulution.java
+++ day3-LongestSubstringWithoutRepeatingCharacters/src/main/java/Sulution.java
@@ -1,3 +1,37 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class Sulution {
+  public int lengthOfLongestSubstring(String s) {
+
+    //abcabcbb  3   4
+    //dvdff   3
+    //pwwkew  3
+    if(s.length()==0){
+      return 0;
+    }
+    int left = 0;
+    int longestLength = 1;
+    Set<Character> tempSet =new HashSet<Character>();
+    tempSet.add(s.charAt(0));
+
+    for(int right=1;right<s.length();right++){
+      if(!tempSet.contains(s.charAt(right))){
+        tempSet.add(s.charAt(right));
+        longestLength = Math.max(longestLength, right - left + 1);
+      }else{
+        while (tempSet.contains(s.charAt(right))) {
+          tempSet.remove(s.charAt(left));
+          left++;
+        }
+        tempSet.add(s.charAt(right));
+      }
+    }
+
+    return longestLength;
+  }
 
 }
