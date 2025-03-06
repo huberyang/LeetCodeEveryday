308573983df2ea64cf9fe3106d71daca977584bf
YangSen <yang.sen@ufinity.com>
2025-03-06 11:35:35 +0800

    [NeetCode][BaseProjStructure] self completed

--- NeetCode/1Assays&Hashing/GroupAnagrams/src/main/java/solution/MySoulution.java
+++ NeetCode/1Assays&Hashing/GroupAnagrams/src/main/java/solution/MySoulution.java
@@ -1,6 +1,10 @@
 package solution;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MySoulution {
 
@@ -11,9 +15,17 @@ public class MySoulution {
    * @param strs
    * @return
    */
-  public List<List<String>> groupAnagrams(String[] strs) {
-
-    return null;
+  public static List<List<String>> groupAnagrams(String[] strs) {
+    Map<String, ArrayList<String>> tempMap = new HashMap<>();
+    for(String data:strs){
+      char temp[] = data.toCharArray();
+      Arrays.sort(temp);
+      String sortedS = new String(temp);
+      tempMap.putIfAbsent(sortedS,new ArrayList<>());
+      tempMap.get(sortedS).add(data);
+    }
+    
+    return new ArrayList<>(tempMap.values());
   }
 
 }
--- NeetCode/1Assays&Hashing/GroupAnagrams/src/test/java/TestCase.java
+++ NeetCode/1Assays&Hashing/GroupAnagrams/src/test/java/TestCase.java
@@ -1,10 +1,31 @@
+import java.util.List;
 import org.junit.Test;
+import solution.MySoulution;
 
 public class TestCase {
   
   @Test
   public void testSolution(){
     
+    String[] testDate = {"act","pots","tops","cat","stop","hat"};
+    System.out.println(MySoulution.groupAnagrams(testDate));
+    
+  }
+
+  @Test
+  public void testSolution2(){
+
+    String[] testDate = {"x"};
+    System.out.println(MySoulution.groupAnagrams(testDate));
+
+  }
+
+  @Test
+  public void testSolution3(){
+
+    String[] testDate = {""};
+    System.out.println(MySoulution.groupAnagrams(testDate));
+
   }
 
 }
