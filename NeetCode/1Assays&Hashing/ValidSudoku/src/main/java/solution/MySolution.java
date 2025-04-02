11f79213fed28c749deee7a2cf0c0c8eb2fa464e
YangSen <yang.sen@ufinity.com>
2025-04-02 15:23:00 +0800

    refactor proj structure

---
+++ NeetCode/1Assays&Hashing/ValidSudoku/src/main/java/solution/MySolution.java
@@ -0,0 +1,17 @@
+package solution;
+
+public class MySolution {
+
+
+  /**
+   * Time Complexity: 
+   * Space Complexity: 
+   * @param board
+   * @return
+   */
+  public boolean isValidSudoku(char[][] board) {
+
+    return true;
+  }
+
+}
